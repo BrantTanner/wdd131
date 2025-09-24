94d7c628f1691eddb1e26de7382015d0c4be5379
BrantTanner <brant.tanner123@gmail.com>
2025-09-24 14:45:11 -0600

    put files in seperate folders, did ponder02

--- ponder/meta-css.js
+++
@@ -1,7 +0,0 @@
-
-let selectElem = document.getElementById('webdevlist');
-selectElem.addEventListener('change', function(){
-    let codeValue = selectElem.value;
-    console.log(codeValue);
-})
-                
